3a7e7b2b4e4026ebbd785ca5836f36ac0ab6e106
Hua Phuoc <hua.phuoc@mmulodo.com>
2018-01-12 08:58:06 +0700

    change code by code rules

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,7 +1,7 @@
 Post.delete_all
 10.times do
-     Post.create(
-        title: FFaker::Book.title,
-        content: FFaker::Book.description
-      ) 
+  Post.create(
+    title: FFaker::Book.title,
+    content: FFaker::Book.description
+  ) 
 end

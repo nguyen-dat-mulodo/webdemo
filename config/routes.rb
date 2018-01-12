599eb6933c3e5528d482af96bf262e3e53a18a70
Hua Phuoc <hua.phuoc@mmulodo.com>
2018-01-12 15:08:44 +0700

    add bootstrap and show list posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'post/index'
+
 	get '/welcome' => 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

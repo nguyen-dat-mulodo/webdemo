4fd7a3b7c4344ce5241940f40f081c67f2ccc88a
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-15 12:45:20 +0700

    Create module post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'post/index'
+  resources :post
   root to: 'post#index'
 	get '/welcome' => 'pages#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

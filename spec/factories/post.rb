dea1fa0f763eb211090fcf03d2576218968c9740
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-17 17:16:18 +0700

    change faker random to ffaker random

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
   factory :post do
     title { FFaker::Book.title }
     content { FFaker::Book.description }
-    quantity { 5 }
+    quantity { FFaker::Random.rand(1..100) }
   end
 end

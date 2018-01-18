dfd5c8976a7b78ee463e1f425299d92a55777424
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-18 08:33:02 +0700

    fix post mode spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,22 +22,23 @@ RSpec.describe Post, type: :model do
   end
 
   it "is invalid if Title is too short (minimum is 5 characters)" do
-    post = FactoryGirl.build(:post, title: "Post title 1", content: "Post content 1", quantity: 1)
-    expect(post.title.length).not_to be < 5
+    post = FactoryGirl.build(:post, title: "Post", content: "Post content 1", quantity: 1)
+    expect(post).not_to be_valid
   end
 
   it "is invalid if Content is too short (minimum is 10 characters)" do
-    post = FactoryGirl.build(:post, title: "Post title 1", content: "Post content 1", quantity: 2)
-    expect(post.content.length).not_to be < 10
+    post = FactoryGirl.build(:post, title: "Post title 1", content: "Post", quantity: 2)
+    expect(post).not_to be_valid
   end
 
   it "is invalid if Title has already been taken" do
-    post = FactoryGirl.build(:post, title: "Post title 2", content: "Post content 1", quantity: 3)
-    expect(post.title).not_to be("Post title 1")
+    exist_post = FactoryGirl.create(:post, title: "Post title 1", content: "Post content 1", quantity: 3)
+    new_post = FactoryGirl.build(:post, title: "Post title 1", content: "Post content 2", quantity: 4)
+    expect(new_post).not_to be_valid
   end
 
   it "is invalid if Quantity is not integer" do
-    post = FactoryGirl.build(:post, title: "Post title 1", content: "Post content 1", quantity: 4)
-    expect(post.quantity).to be_an(Integer)
+    post = FactoryGirl.build(:post, title: "Post title 1", content: "Post content 1", quantity: 'abc')
+    expect(post).not_to be_valid
   end
 end

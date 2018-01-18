774fa84279bcc2707fcf8913d524da12de744ec8
Vo Thanh <vo.thanh@mulodo.com>
2018-01-18 16:56:08 +0700

    [training-routing] Added demos for routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  private
+    def authorize_admin!
+       user_signed_in?
+       
+       unless current_user.admin?
+            flash[:alert] = "You must be an admin to do that."
+            redirect_to root_path
+       end
+    end
 end

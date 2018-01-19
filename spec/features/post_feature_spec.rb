ecfc7b01890561f34f7b5e51043c581bac2275eb
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-19 17:08:24 +0700

    [training-test] - Add features: show, update, destroy to post_feature_spec.rb

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -7,6 +7,13 @@ feature "#Post management" do
     find('ul[@class="menu"]').click_link("Post")
   end
 
+  given!(:post)    { Post.create!(title: 'Post title 1', content: 'Post content 1.', quantity: 1) }
+
+  scenario "show" do
+    visit posts_path
+    click_link 'Show'
+  end
+
   scenario "create" do
     sign_in
     visit '/posts'
@@ -26,6 +33,31 @@ feature "#Post management" do
     expect(find('form#new_post')).to have_button('Create Post')
     find('form#new_post').click_button("Create Post")
     expect(page).to have_text("Post was successfully created.")
+  end
+
+  given(:new_post) { Post.new(title: 'Post title 2', content: 'Post content 2.', quantity: 2) }
+  
+  scenario 'update' do
+    sign_in
+    visit posts_path
+    click_link 'Edit'
+
+    within("form.edit_post") do
+      fill_in 'Title', with: new_post.title
+      fill_in 'Content', with: new_post.content
+      fill_in 'Quantity', with: new_post.quantity
+    end
+
+    click_button 'Update Post'
+    expect(page).to have_content 'Post was successfully updated.'
+  end
+
+  scenario 'destroy' do
+    sign_in
+    visit posts_path
+    click_link 'Delete'
+    expect(page).to have_content 'Post was successfully destroyed.'
+    expect(page).not_to have_content post.title
   end
 
 end

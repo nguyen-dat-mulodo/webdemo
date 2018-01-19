099f9433f070858faa9c999b61449e5bb879ff33
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-19 16:50:02 +0700

    [training-test] - Modify code user_feature_spec.rb and psot_feature_spec.rb

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -2,19 +2,29 @@ require "rails_helper"
 
 feature "#Post management" do
   scenario "index" do
-    visit root_path
-    click_link "Post"
+    visit '/'
+    expect(find('ul[@class="menu"]')).to have_link('Post')
+    find('ul[@class="menu"]').click_link("Post")
   end
 
   scenario "create" do
     sign_in
-    visit new_post_path
-    within("#new_post") do
+    visit '/posts'
+
+    expect(find('div.container')).to have_link('New Post')
+    find('div.container').click_link("New Post")
+
+    within("form#new_post") do
+      expect(find('div[@class="form-group"]/label[@for="post_title"]')).to have_text('Title')
       fill_in "Title", :with => "Post title 10"
+      expect(find('div[@class="form-group"]/label[@for="post_content"]')).to have_text('Content')
       fill_in "Content", :with => "Post content 10"
+      expect(find('div[@class="form-group"]/label[@for="post_quantity"]')).to have_text('Quantity')
       fill_in "Quantity", :with => 10
     end
-    click_button "Create Post"
+
+    expect(find('form#new_post')).to have_button('Create Post')
+    find('form#new_post').click_button("Create Post")
     expect(page).to have_text("Post was successfully created.")
   end
 

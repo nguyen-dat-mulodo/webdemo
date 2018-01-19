98c947e4bdfc32730ce63ec51fd7c555421b3a98
Hua Phuoc <hua.phuoc@mulodo.com>
2018-01-19 08:55:34 +0700

    [Training ROR Test] - Post controller test

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -31,8 +31,8 @@ RSpec.describe PostsController, :type => :controller do
 
   describe "#create" do
     login_user
-    new_post = {title: 'Post title', content: 'Post content', quantity: 1}
     context "when attributes is valid" do
+      new_post = { title: 'Post title', content: 'Post content', quantity: 1 }
       it "creates a new post" do
         expect do
           post :create, params: { post: new_post }
@@ -42,6 +42,65 @@ RSpec.describe PostsController, :type => :controller do
       it "redirects to the post" do
         post :create, params: { post: new_post }
         expect(response).to redirect_to Post.last
+      end
+    end
+
+    context "when attributes is invalid" do
+      invalid_post = { title: nil, content: 'Post content', quantity: 1 }
+      it "does not save the new post" do
+        expect do
+          post :create, params: { post: invalid_post }
+        end.to_not change(Post,:count)
+      end
+      
+      it "re-renders the new method" do
+        post :create, params: { post: invalid_post }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe '#update' do
+    login_user
+    before :each do
+      @post = FactoryGirl.create(:post)
+    end
+    
+    context "when attributes is valid" do
+      it "assigns requested @post" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post) }
+        expect(assigns(:post)).to eq(@post)      
+      end
+    
+      it "changes attributes" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post, title: "Post title 2", content: "Post content 2") }
+        @post.reload
+        expect(@post.title).to eq("Post title 2")
+        expect(@post.content).to eq("Post content 2")
+      end
+    
+      it "redirects to the updated" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post) }
+        expect(response).to redirect_to @post
+      end
+    end
+    
+    context "when attributes is valid invalid" do
+      it "assigns requested @post" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post, title: nil, content: nil) }
+        expect(assigns(:post)).to eq(@post)      
+      end
+      
+      it "does not change @post" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post, title: nil, content: nil) }
+        @post.reload
+        expect(@post.title).not_to eq("Post title 3")
+        expect(@post.content).not_to eq("Post content 3")
+      end
+      
+      it "re-renders the edit method" do
+        put :update, params: { id: @post, post: FactoryGirl.attributes_for(:post, title: nil, content: nil) }
+        expect(response).to render_template :edit
       end
     end
   end
